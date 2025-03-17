2e5fdef767ca931d7dd2d5bbcaebfc014c01442e
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2025-03-17 23:23:14 +0530

    Update hero.jsx

--- src/hero.jsx
+++ src/hero.jsx
@@ -284,7 +284,7 @@ const Hero = () => {
 
   return (
     <section 
-      className={` h-[1000px] relative w-full flex items-center justify-center bg-gradient-to-b from-black to-blue-900`}
+      className={` max-h-screen relative w-full flex items-center justify-center bg-gradient-to-b from-black to-blue-900`}
       style={{ height: canvasHeight }}
     >
       <ErrorBoundary fallback={<HeroFallback isLowPerformance={true} />}>
